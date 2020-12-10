42b639bdee0c4e7bb48e283b7f3d349ca0e361b5
root <root@LAPTOP-EA6PCKJ1.localdomain>
2020-12-09 20:02:31 -0800

    added a new folder for the sign up page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Button } from 'react-bootstrap';
+import { Route, useHistory } from 'react-router';
+import { Link } from 'react-router-dom';
 import './App.css';
+import SignUp from './forms/signup'
+
+
+
+function form(): string{
+
+  let route = './forms/signup'
+
+      return route
+  
+
+}
 
 function App() {
-  return (
+  let history = useHistory();
+
+  const redirect = () => {
+    history.push(form())
+  }
+  return <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <Button  onClick = {redirect}  type = "submit" variant="danger" block > Sign Up!</Button> 
+
     </div>
-  );
+  </>
 }
 
 export default App;
