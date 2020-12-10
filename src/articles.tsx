8a7e7a173d848e1a047ab7db2bbe35fc0d9c7c48
DanielleAtherfold <datherfold16578@alumni.focuscollege.com>
2020-12-09 20:33:38 -0800

    started the articles page

---
+++ src/articles.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+<h1>Our Articles</h1>
+const BorderPage = () => {
+    return (
+    <>
+        <span className="block-example border border-dark"></span>
+        <span className="block-example border-top border-dark"></span>
+        <span className="block-example border-right border-dark"></span>
+        <span className="block-example border-bottom border-dark"></span>
+        <span className="block-example border-left border-dark"></span>
+    </>
+    );
+    }
+    
+    export default BorderPage;
