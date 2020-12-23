8f7d443673bcab443ba838e93512761eaa8736e0
Colby <cboettcher16590@alumni.focuscollege.com>
2020-12-22 16:13:47 -0800

    improved styling in sign-up and sign-in

--- webapp/src/pages/forms/signin.tsx
+++ webapp/src/pages/forms/signin.tsx
@@ -3,30 +3,6 @@ import MainLayout from "../../layouts/MainLayout";
 import { useAuthentication } from '../../data/useAuthentication';
 import { Button } from "react-bootstrap";
 
-function randomRedirect():string{
-
-    let randomNumber = Math.floor(Math.random() * 10 );
-    let route3 ='/OrgHome';
-
-      //if randomNumber is greater than 5 it will redirect to success
-      //otherwise it will redirect to failure
-      if(randomNumber>=5){
-        let route = "/memberHome"
-        return route;
-      } 
-      if(randomNumber>=4){
-        let route = "/adminHome"
-        return route;
-      } 
-      if(randomNumber>=3){
-        let route = "/authorHome"
-        return route;
-      } 
-      else{
-        
-        return route3;
-      }
-  }
 
 // function compaireEmails(memberData():any, DB.dbSignIn():any){
 //   if(memberData === DB.dbSignIn){
@@ -47,8 +23,8 @@ export default function Login() {
     return <>
 <MainLayout>
 
-    <form>
-                <h3>Sign In</h3>
+    <form id="signForm">
+                <h2>Sign In</h2>
 
                 <div className="form-group">
                     <label>Email </label>
@@ -62,8 +38,8 @@ export default function Login() {
                     <label>Password </label>
                     <input type="password" className="form-control" placeholder="Enter password" />
                 </div>
-
-                <Button onClick = {Login} type="submit" className="btn btn-primary btn-block">
+                {/* <Button onClick = {Login} type="submit" className="btn btn-primary btn-block"> */}
+                <Button type="submit" className="btn btn-primary btn-block">
                     Sign In
                 </Button>
                 <p className="forgot-password text-right">
