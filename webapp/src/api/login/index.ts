15b7c6f1babc16ccc9a7b8adf4f5a73f232a2ab1
Mikita Krahelski <nikita.krahelski@gmail.com>
2021-01-30 20:44:34 -0800

    Installed 3.8 typescript. This download to prevent an accordian error. Either that or downgrad bootstrap. Unsure why the error is caused. Axios import needed to be reset. Unsure why.

--- webapp/src/api/login/index.ts
+++ webapp/src/api/login/index.ts
@@ -15,12 +15,18 @@ export interface IUserLogin {
 }
 
 export default {
-    post: async (body:any) => {
+    post: async (body:any) => { //possible a get instead of a post? //where does it post too.
         //lets us see what is coming in
         console.log("BODY:  ", body);
         
+        //so we want to get a list of users. Then we want to do a post to local.storage
+
         axios.post(`${HOSTNAME}/users`, body).then(responce => {
             let data:IUserLogin = JSON.parse(JSON.stringify(responce.data[0]));
+        //we grab data using Iuser = json, which you parse. You take the 0 array member. Then store it
+        //local storage
+
+        console.log(data)
 
             localStorage.setItem("firstName", data.fName);
             localStorage.setItem("lastName", data.lName);
