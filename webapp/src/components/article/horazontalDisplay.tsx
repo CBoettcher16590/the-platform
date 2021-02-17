fd5079d5c60727b03b269858ce9f1aaa993d4ab5
Colby <cboettcher16590@alumni.focuscollege.com>
2021-02-16 18:02:02 -0800

    Got the article Titles in the horazontal div redirecting to the correct article page

--- webapp/src/components/article/horazontalDisplay.tsx
+++ webapp/src/components/article/horazontalDisplay.tsx
@@ -1,21 +1,27 @@
 import React, { useEffect, useState } from 'react';      
 import { CardDeck, Card } from 'react-bootstrap';
+import { useHistory } from 'react-router';
 import { IArticle } from '../../../../services/crud-server/src/models/article';
-
-// import  {IUserLogin} from '../../api/login/index';
 import api from '../../api';
 import PayButton from '../PayButton';
 
 export default function HorazontalDisplay(){
 
 const [articles, setArticles] = useState<IArticle[]>();
+const history = useHistory();
 
   useEffect(() => {
     api.articles.get().then((responce) => {
     setArticles(responce.data);
     }).catch((error: any) => console.error(`Error: ${error}`)); 
         },[]);
- 
+  
+    const GoToArticle = (article:IArticle) => (event:any) => {
+      //here we find the article id for our Title, Link
+      let articleId = article.article_id;
+      //then We use history.push to redirect to that page
+      history.push(`/article/${articleId}`)
+    }
 
 return <>
 <section className="homeStories">
@@ -32,7 +38,7 @@ return <>
 
       <div className="article">
 
-        <h2>{title}</h2>
+        <h2 onClick={GoToArticle(_art)}>{title}</h2>
 
         <p>{preview}</p>
 
