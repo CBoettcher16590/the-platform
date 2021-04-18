94a82bdce1c58960117c3375b59f2b076554c03a
Colby <cboettcher16590@alumni.focuscollege.com>
2021-04-17 19:25:08 -0700

    implemented a usertype check on home page where it first makes sure a user is logged in. Then sets a myAccount variable depending on userType

--- services/crud-server/src/models/token.ts
+++ services/crud-server/src/models/token.ts
@@ -5,14 +5,13 @@ dotenv.config();
 export const TokenModel = {
 
     generateAccessToken: ( payload:any ) => {
-        return jwt.sign( payload, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '7200s' }); //Made this really short just for testing purposes
+        return jwt.sign( payload, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '43000s' }); //Made this really short just for testing purposes
     },
 
     validateToken: ( token:string, onError:(err:any)=>any, onSuccess:(payload:any)=>any ) => {
         jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, ( err:any, payload:any ) => {
             
             if( err ){
-                localStorage.clear();
                 onError(err);
                 console.log("Token Validation Error");
                 return;
