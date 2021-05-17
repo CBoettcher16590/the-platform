9ad7785e47b99a2ffee4aa5f6efd502d049ee502
Mikita Krahelski <mkrahelski16610@alumni.focuscollege.com>
2021-05-17 13:53:25 -0700

    Slight edit. The buttons disable on click and pop up you can only vote once on an article. This can easily be rolled back. It functions similarly to the previous system. It probably updates the DB but haven't tested on unrated articles. Works similarly without the refresh.

--- webapp/src/components/article/indvArticle.tsx
+++ webapp/src/components/article/indvArticle.tsx
@@ -24,7 +24,8 @@ const IndvArticle = () => {
     const history = useHistory();
     const loggedInUserId = localStorage.getItem("userID") || "";
     const [loggedInUser, setLoggedInUser] = useState<IUser>();
-    const [buttonDisabled, setButtonDisabled] = useState(false);
+    const [buttonDisabledA, setButtonDisabledA] = useState(false);
+
 
 
     const [article, setArticle] = useState<IArticle>();
@@ -41,15 +42,17 @@ const IndvArticle = () => {
 
 
     const HandelRating = (userRating: number) => (event: any) => {
-        setButtonDisabled(true);
+        
         event.preventDefault();
+        setButtonDisabledA(true);
         //First we look to see if the user has already rated this article with the following api call
         api.rating.get().then((responce) => {
             let dupRating: IRating[] = responce.data.filter((_rating: IRating) => _rating.user_id === parseInt(loggedInUserId) && _rating.article_id === article!.article_id);
             
+            
 
             if (dupRating[0]) {
-                // alert("You Can Only Rate An Article Once!");
+                alert("You Can Only Rate An Article Once!");
             } else {
                 //First Add New Info To Ratings Table
                 api.rating.post({ article_id: article!.article_id, user_id: loggedInUserId, rating: userRating }).then(responce => {
@@ -117,13 +120,13 @@ const IndvArticle = () => {
                     </Col>
                     <div className="buttonsRatingNoSeries">
                         <div className="slyButtonSpacing">
-                            <Button onClick={HandelRating(1)} disabled={buttonDisabled} size="sm" variant="outline-success" ><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <Button onClick={HandelRating(1)} disabled={buttonDisabledA} size="sm" variant="outline-success" ><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M17.25 10.25L12 4.75L6.75 10.25" stroke="#141414" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
                                 <path d="M12 19.25V5.75" stroke="#141414" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
                             </svg> </Button>{' '}
                         </div>
                         <div>
-                            <Button onClick={HandelRating(-1)} disabled={buttonDisabled} size="sm" variant="outline-danger"> <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <Button onClick={HandelRating(-1)} disabled={buttonDisabledA} size="sm" variant="outline-danger"> <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M17.25 13.75L12 19.25L6.75 13.75" stroke="#141414" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
                                 <path d="M12 18.25V4.75" stroke="#141414" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
                             </svg> </Button>{' '}
